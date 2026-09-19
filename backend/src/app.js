4ed9ec8e42c64c98220aacedab1ebb5eed977695
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-09-19 10:18:59 +0530

    url change

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://sat-query-ai-pied.vercel.app/login",
     credentials: true,
   })
 );
